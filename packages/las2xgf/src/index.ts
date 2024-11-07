dbd997a3758714652a5761fcc9620c5dccc7db30
lindsay <lindsay.kay@xeolabs.com>
2024-11-07 22:00:45 +0100

    Refactorings

--- packages/las2xgf/src/index.ts
+++ packages/las2xgf/src/index.ts
@@ -1,14 +1,14 @@
 /**
  * [![npm version](https://badge.fury.io/js/%40xeokit%2Fifcviewer.svg)](https://badge.fury.io/js/%40xeokit%2Fifcviewer)
- * [![](https://data.jsdelivr.com/v1/package/npm/@xeokit/las2dtx/badge)](https://www.jsdelivr.com/package/npm/@xeokit/ifcviewer)
+ * [![](https://data.jsdelivr.com/v1/package/npm/@xeokit/las2xgf/badge)](https://www.jsdelivr.com/package/npm/@xeokit/ifcviewer)
  *
  * <img style="padding:0px; padding-top:30px; padding-bottom:30px;" src="media://images/tree_view_icon.png"/>
  *
  * <br>
  *
- * ## las2dtx
+ * ## las2xgf
  *
- * Node CLI tool to convert LAS files into [DTX](https://xeokit.github.io/sdk/docs/pages/GLOSSARY.html#dtx) files.
+ * Node CLI tool to convert LAS files into [XGF](https://xeokit.github.io/sdk/docs/pages/GLOSSARY.html#xgf) files.
  *
  * ## Features
  *
@@ -17,7 +17,7 @@
  * ## Installation
  *
  * ````bash
- * npm install @xeokit/las2dtx
+ * npm install @xeokit/las2xgf
  * ````
  *
  * ## Usage
@@ -25,9 +25,9 @@
  * TODO
  *
  * ````bash
- * las2dtx -i model.json -o model.dtx
+ * las2xgf -i model.json -o model.xgf
  * ````
  *
- * @module @xeokit/las2dtx
+ * @module @xeokit/las2xgf
  */
-export * from "./las2dtx"
+export * from "./las2xgf"
